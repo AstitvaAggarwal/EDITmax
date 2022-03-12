cacd1f9ad005c0ecad9205174c90237827f93b11
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2022-03-13 01:04:16 +0530

    updated script.js

--- templates/script.js
+++ templates/script.js
@@ -9,6 +9,8 @@ let uploadButton = document.getElementById("upload-button");
 let DownloadButton = document.getElementById("Download-button");
 let resetfilterchoice=document.getElementById("reset-button");
 let image = document.getElementById("chosen-image");
+let filterOPENCV1=document.getElementById("opencvfilter1");
+
 function resetFilter(){
     filterA.value = "0";
     filterB.value = "100";
@@ -19,28 +21,54 @@ function resetFilter(){
     flipImage();
 }
 
-
 uploadButton.onchange = () => {
     resetFilter();
-    document.querySelector(".image-container").
-    style.display = "block";
+    document.querySelector(".image-container").style.display = "block";
     let reader = new FileReader();
     reader.readAsDataURL(uploadButton.files[0]);
     reader.onload = () => {
         image.setAttribute("src", reader.result);
+        img = cv.imread(image);
+        cv.imshow('myCanvas', img);
     }
+    
 }
+
 let sliders = document.querySelectorAll(".filter input[type='range']");
 sliders.forEach( slider => {
     slider.addEventListener("input", addFilter);
 });
+
 function addFilter(){
     image.style.filter = `blur(${filterA.value}px) contrast(${filterB.value}%) hue-rotate(${filterC.value}deg) sepia(${filterD.value}%)`;
 }
+
+let checked = document.querySelectorAll(".cusfilter input[type='checkbox']");
+checked.forEach( checker => {
+    checker.addEventListener('change', addcusFilter);
+});
+
+function addcusFilter(){
+    //render image anyways
+    img = cv.imread(image);
+    cv.imshow('myCanvas', img);
+
+    //if checked then apply filter only
+    if (this.checked) {
+        console.log("This is Checked")
+        let dst = new cv.Mat();
+        img = cv.imread(image)
+        cv.Canny(img, dst, 50, 100, 3, false);
+        cv.imshow('myCanvas', dst);
+        dst.delete();
+    }
+}
+
 let radioBtns = document.querySelectorAll(".flip-option input[type='radio']");
 radioBtns.forEach( radioBtn => {
     radioBtn.addEventListener("click", flipImage);
 });
+
 function flipImage(){
     if(flipXBtn.checked){
         image.style.transform = "scaleX(-1)";
